c289044ac078e70960ee3efdf81adff5103b4e1e
Kous <kous.takken@gmail.com>
2023-07-09 13:26:52 +0300

    add router

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { userRouter } from './users/users.js';
 
 const port = 8000;
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('hello express');
 });
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log(`server start on http//localhost:${port}`);
